8aef7d9529d71d4e90e9ac468139edaf95db292f
harini <harinisiva1997@gmail.com>
2020-12-15 00:26:29 +0530

    if no ingredients

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -3,14 +3,20 @@ import classes from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
     //js function that returns aray of keys
-    const tranformedIngredients = Object.keys(props.ingredients)
+    let tranformedIngredients = Object.keys(props.ingredients)
         .map(igkey => {
             console.log(igkey)
             return [...Array(props.ingredients[igkey])].map((_, i) =>{
+                console.log(i)
                     return <BurgerIngredient key={igkey + i} type={igkey} />
                 } )
         })
+        .reduce((arr,el)=>{ console.log(arr) 
+            return arr.concat(el) },[])
 
+    if (tranformedIngredients.length === 0){
+        tranformedIngredients=<p>Please start adding ingredients</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
