eacbb627eac335a17d6bbc8e166851ddc048e3d7
harini <harinisiva1997@gmail.com>
2020-12-14 23:03:19 +0530

    adding dynamic ingredient component and Prop type

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,9 +1,9 @@
 import Aux from '../../hoc/Auxillary'
-import Classes from './Layout.module.css'
+import classes from './Layout.module.css'
 const layout = (props) =>(
     <Aux> 
     <div>Toolbar, SideBar, Backdrop</div>
-    <main className={Classes.content}>{props.children}</main>
+    <main className={classes.content}>{props.children}</main>
     </Aux>
 )
 export default layout
