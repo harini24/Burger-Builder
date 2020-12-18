4278fea30b8c5b68e1bb4c96a1e25d4183f99d64
harini <harinisiva1997@gmail.com>
2020-12-18 18:53:03 +0530

    dynamic burger build control with less button diability

--- src/Components/Burger/BuildControls/BuildControls.js
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -11,7 +11,11 @@ const buildControls = props => {
     return(
         <div className={classes.BuildControls}>
             {controls.map(ctrl=>
-                (<BuildControl key={ctrl.label} label={ctrl.label}></BuildControl>))}
+                (<BuildControl key={ctrl.label} label={ctrl.label} 
+                added={()=>props.ingredientAdded(ctrl.type)}
+                removed={()=>props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}></BuildControl>))}
+                
         </div>
     )
 }
