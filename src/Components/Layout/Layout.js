9769e2b0c731a71ed593658908ef0c15c5abc86e
harini <harinisiva1997@gmail.com>
2020-12-25 12:47:24 +0530

    sidebar first step

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import Aux from '../../hoc/Auxillary'
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/Sidedrawer'
 const layout = (props) => (
     <Aux>
         <Toolbar/>
+        <SideDrawer/>
         <main className={classes.content}>{props.children}</main>
     </Aux>
 )
---
+++ src/Components/Navigation/SideDrawer/Sidedrawer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from '../../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems'
+import classes from './Sidedrawer.module.css'
+const sidedrawer = props => {
+    return(
+        <div className={classes.Sidedrawer}>
+            <Logo height="11%"/>
+            <nav>
+
+            <NavigationItems/>
+            </nav>
+        </div>
+    );
+}
+export default sidedrawer
