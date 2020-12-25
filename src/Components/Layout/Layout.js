ae0554557495cf0703ffb5dfc0734ced99892275
harini <harinisiva1997@gmail.com>
2020-12-25 14:54:46 +0530

    sidebar with backdrop

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,12 +1,23 @@
+import react, {Component} from 'react'
 import Aux from '../../hoc/Auxillary'
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/Sidedrawer'
-const layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.content}>{props.children}</main>
-    </Aux>
-)
-export default layout
+class Layout extends Component {
+    state={
+        showSideDrawer:true
+    }
+    sideDrawerClosedhandler = () => {
+        this.setState({showSideDrawer:false})
+    }
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer show={this.state.showSideDrawer} closed={this.sideDrawerClosedhandler} />
+                <main className={classes.content}>{this.props.children}</main>
+            </Aux>
+        )
+    }
+}
+export default Layout
--- src/Components/Navigation/SideDrawer/Sidedrawer.js
+++ src/Components/Navigation/SideDrawer/Sidedrawer.js
@@ -2,16 +2,25 @@ import React from 'react'
 import Logo from '../../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems'
 import classes from './Sidedrawer.module.css'
+import BackDrop from '../../UI/BackDrop/Backdrop'
+import Aux from '../../../hoc/Auxillary'
 const sidedrawer = props => {
-    return(
-        <div className={classes.Sidedrawer}>
-            <div className={classes.Logo}>
-            <Logo />
+    let attachedClasses=[classes.Sidedrawer,classes.Close]
+    if (props.show){
+        attachedClasses = [classes.Sidedrawer,classes.Open]
+    }
+    return (
+        <Aux>
+            <BackDrop show={props.show} clicked={props.closed}/>
+            <div className={attachedClasses.join(" ")}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-            <NavigationItems/>
-            </nav>
-        </div>
+        </Aux>
     );
 }
 export default sidedrawer
